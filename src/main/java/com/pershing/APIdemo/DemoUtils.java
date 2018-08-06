e6032d82f0c4af945e9dc208942d26c85b9e9fab
Ian <ianw3214@gmail.com>
2018-08-06 04:23:10 -0400

    Redesign foreign exchange rate to show rates directly on image carousel

--- src/main/java/com/pershing/APIdemo/DemoUtils.java
+++ src/main/java/com/pershing/APIdemo/DemoUtils.java
@@ -7,6 +7,7 @@ import com.pershing.action.PostbackAction;
 import com.pershing.action.URIAction;
 import com.pershing.message.Message;
 import com.pershing.message.TemplateMessage;
+import com.pershing.mockAPI.MockAPI;
 import com.pershing.sender.MessageSender;
 import com.pershing.template.ButtonsTemplate;
 import com.pershing.template.CarouselTemplate;
@@ -20,27 +21,32 @@ public class DemoUtils {
 		// Construct the image carousel
 		CarouselTemplate carousel = new CarouselTemplate();
 		// EUR currency
-		Column EUR_column = new Column("歐元 EUR");
+		float EUR = MockAPI.getCurrency("EUR", "TWD");
+		Column EUR_column = new Column("歐元 EUR: " + Float.toString(EUR));
 		EUR_column.setThumbnailImageUrl("https://i.imgur.com/GObxdWf.jpg");
 		EUR_column.addAction(new PostbackAction("我要兌換", "action=exchange&data=EUR", "\u200B" + "兌換歐元"));
 		carousel.addColumn(EUR_column);
 		// JPY currency
-		Column JPY_column = new Column("日元 JPY");
+		float JPY = MockAPI.getCurrency("JPY", "TWD");
+		Column JPY_column = new Column("日元 JPY: " + Float.toString(JPY));
 		JPY_column.setThumbnailImageUrl("https://i.imgur.com/1qE09cD.jpg");
 		JPY_column.addAction(new PostbackAction("我要兌換", "action=exchange&data=JPY", "\u200B" + "兌換日元"));
 		carousel.addColumn(JPY_column);
 		// CAD currency
-		Column CAD_column = new Column("加幣 CAD");
+		float CAD = MockAPI.getCurrency("CAD", "TWD");
+		Column CAD_column = new Column("加幣 CAD: " + Float.toString(CAD));
 		CAD_column.setThumbnailImageUrl("https://i.imgur.com/6FavWOy.jpg");
 		CAD_column.addAction(new PostbackAction("我要兌換", "action=exchange&data=CAD","\u200B" + "兌換加幣"));
 		carousel.addColumn(CAD_column);
 		// CNY currency
-		Column CNY_column = new Column("人民幣 CNY");
+		float CNY = MockAPI.getCurrency("CNY", "TWD");
+		Column CNY_column = new Column("人民幣 CNY: " + Float.toString(CNY));
 		CNY_column.setThumbnailImageUrl("https://i.imgur.com/Egu251z.jpg");
 		CNY_column.addAction(new PostbackAction("我要兌換", "action=exchange&data=CNY", "\u200B" + "兌換人民幣"));
 		carousel.addColumn(CNY_column);
 		// USD currency
-		Column USD_column = new Column("美元 USD");
+		float USD = MockAPI.getCurrency("USD", "TWD");
+		Column USD_column = new Column("美元 USD: " + Float.toString(USD));
 		USD_column.setThumbnailImageUrl("https://i.imgur.com/gmxsIO9.jpg");
 		USD_column.addAction(new PostbackAction("我要兌換", "action=exchange&data=USD", "\u200B" + "兌換美元"));
 		carousel.addColumn(USD_column);
